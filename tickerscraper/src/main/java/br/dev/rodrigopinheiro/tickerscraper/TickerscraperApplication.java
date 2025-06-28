6043e515b72c03a392c32c3df05e57ac4f5cc3c1
rpsouza441 <rpsouza@hotmail.co.uk>
2025-06-28 18:36:25 -0300

    initial commit

---
+++ tickerscraper/src/main/java/br/dev/rodrigopinheiro/tickerscraper/TickerscraperApplication.java
@@ -0,0 +1,13 @@
+package br.dev.rodrigopinheiro.tickerscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickerscraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickerscraperApplication.class, args);
+	}
+
+}
---
+++ tickerscraper/src/test/java/br/dev/rodrigopinheiro/tickerscraper/TickerscraperApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.rodrigopinheiro.tickerscraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickerscraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
