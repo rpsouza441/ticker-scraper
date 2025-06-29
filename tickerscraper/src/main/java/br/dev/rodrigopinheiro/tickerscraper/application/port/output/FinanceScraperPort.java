4bcc0bffa89860a40db31b8d4dbb350610dffb33
rpsouza441 <rpsouza@hotmail.co.uk>
2025-06-29 16:59:02 -0300

    Create FinanceScraperPort.java

---
+++ tickerscraper/src/main/java/br/dev/rodrigopinheiro/tickerscraper/application/port/output/FinanceScraperPort.java
@@ -0,0 +1,9 @@
+package br.dev.rodrigopinheiro.tickerscraper.application.port.output;
+
+import br.dev.rodrigopinheiro.tickerscraper.domain.model.Acao;
+
+public interface FinanceScraperPort {
+
+    Acao buscarDadosDeAcao(String ticker);
+    //FundoImobiliario buscarDadosDeFii(String ticker);
+}
