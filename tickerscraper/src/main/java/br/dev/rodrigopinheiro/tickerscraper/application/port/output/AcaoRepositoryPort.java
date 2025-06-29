eea3db75cfdbdc289ab5f497a1a9f166a6960e79
rpsouza441 <rpsouza@hotmail.co.uk>
2025-06-29 16:58:51 -0300

    Create AcaoRepositoryPort.java

---
+++ tickerscraper/src/main/java/br/dev/rodrigopinheiro/tickerscraper/application/port/output/AcaoRepositoryPort.java
@@ -0,0 +1,18 @@
+package br.dev.rodrigopinheiro.tickerscraper.application.port.output;
+
+import br.dev.rodrigopinheiro.tickerscraper.adapter.output.persistence.entity.AcaoEntity;
+import br.dev.rodrigopinheiro.tickerscraper.application.dto.PageQuery;
+import br.dev.rodrigopinheiro.tickerscraper.application.dto.PagedResult;
+import br.dev.rodrigopinheiro.tickerscraper.domain.model.Acao;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+public interface AcaoRepositoryPort {
+
+    Acao save(Acao acao);
+    Optional<Acao> findById(Long id);
+    PagedResult<Acao> findAll(PageQuery query);
+}
+
