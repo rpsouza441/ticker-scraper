91cdff6bd92f2cb418e9b2a00b04ec6055a8c1fe
rpsouza441 <rpsouza@hotmail.co.uk>
2025-06-30 21:03:05 -0300

    Adicao de metodo

--- tickerscraper/src/main/java/br/dev/rodrigopinheiro/tickerscraper/adapter/output/persistence/jpa/AcaoJpaReporitoty.java
+++ tickerscraper/src/main/java/br/dev/rodrigopinheiro/tickerscraper/adapter/output/persistence/jpa/AcaoJpaReporitoty.java
@@ -4,8 +4,11 @@ import br.dev.rodrigopinheiro.tickerscraper.adapter.output.persistence.entity.Ac
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface AcaoJpaReporitoty extends JpaRepository<AcaoEntity, Long> {
+    Optional<AcaoEntity> findByTicker(String ticker);
 
 }
