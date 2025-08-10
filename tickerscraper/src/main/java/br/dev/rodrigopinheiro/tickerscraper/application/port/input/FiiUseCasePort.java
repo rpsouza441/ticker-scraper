d7f6d493e99ed083e04c658ca4437a28e7f0aa27
rpsouza441 <rpsouza@hotmail.co.uk>
2025-08-10 18:37:13 -0300

    Update FiiUseCasePort.java

--- tickerscraper/src/main/java/br/dev/rodrigopinheiro/tickerscraper/application/port/input/FiiUseCasePort.java
+++ tickerscraper/src/main/java/br/dev/rodrigopinheiro/tickerscraper/application/port/input/FiiUseCasePort.java
@@ -1,12 +1,13 @@
 package br.dev.rodrigopinheiro.tickerscraper.application.port.input;
 
 import br.dev.rodrigopinheiro.tickerscraper.adapter.output.persistence.entity.AcaoEntity;
+import br.dev.rodrigopinheiro.tickerscraper.adapter.output.persistence.entity.FundoImobiliarioEntity;
 import br.dev.rodrigopinheiro.tickerscraper.infrastructure.scraper.fii.dto.FiiDadosFinanceirosDTO;
 import reactor.core.publisher.Mono;
 
 public interface FiiUseCasePort {
 
-    Mono<AcaoEntity> getTickerData(String ticker);
+    Mono<FundoImobiliarioEntity> getTickerData(String ticker);
 
     Mono<FiiDadosFinanceirosDTO> getRawTickerData(String ticker);
 }
