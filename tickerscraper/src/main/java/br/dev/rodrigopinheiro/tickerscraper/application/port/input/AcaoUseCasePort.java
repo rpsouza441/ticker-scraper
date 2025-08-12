d9b1c313047b551679a13e5656f7d03b311c1b7f
rpsouza441 <rpsouza@hotmail.co.uk>
2025-08-11 21:23:49 -0300

    Alterado o retorno

--- tickerscraper/src/main/java/br/dev/rodrigopinheiro/tickerscraper/application/port/input/AcaoUseCasePort.java
+++ tickerscraper/src/main/java/br/dev/rodrigopinheiro/tickerscraper/application/port/input/AcaoUseCasePort.java
@@ -1,12 +1,12 @@
 package br.dev.rodrigopinheiro.tickerscraper.application.port.input;
 
-import br.dev.rodrigopinheiro.tickerscraper.adapter.output.persistence.entity.AcaoEntity;
+import br.dev.rodrigopinheiro.tickerscraper.domain.model.Acao;
 import br.dev.rodrigopinheiro.tickerscraper.infrastructure.scraper.acao.dto.AcaoDadosFinanceirosDTO;
 import reactor.core.publisher.Mono;
 
 public interface AcaoUseCasePort {
 
-    Mono<AcaoEntity> getTickerData(String ticker);
+    Mono<Acao> getTickerData(String ticker);
 
     Mono<AcaoDadosFinanceirosDTO> getRawTickerData(String ticker);
 }
