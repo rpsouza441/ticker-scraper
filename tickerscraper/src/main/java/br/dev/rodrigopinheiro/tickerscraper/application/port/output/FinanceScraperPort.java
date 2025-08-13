47284e199f01a9baaf7a98f9cf3a19938ea4194d
rpsouza441 <rpsouza@hotmail.co.uk>
2025-08-12 21:22:10 -0300

    Delete FinanceScraperPort.java

--- tickerscraper/src/main/java/br/dev/rodrigopinheiro/tickerscraper/application/port/output/FinanceScraperPort.java
+++
@@ -1,9 +0,0 @@
-package br.dev.rodrigopinheiro.tickerscraper.application.port.output;
-
-import br.dev.rodrigopinheiro.tickerscraper.domain.model.Acao;
-
-public interface FinanceScraperPort {
-//TODO remover
-    Acao buscarDadosDeAcao(String ticker);
-    //FundoImobiliario buscarDadosDeFii(String ticker);
-}
