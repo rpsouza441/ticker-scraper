4f9b61fe1bce92171644eedee9760a8928aade55
rpsouza441 <rpsouza@hotmail.co.uk>
2025-09-24 12:12:39 -0300

    Refatorar indicadores históricos de BDR

--- tickerscraper/src/main/java/br/dev/rodrigopinheiro/tickerscraper/adapter/output/persistence/jpa/bdr/BdrHistoricalIndicatorEntity.java
+++ tickerscraper/src/main/java/br/dev/rodrigopinheiro/tickerscraper/adapter/output/persistence/jpa/bdr/BdrHistoricalIndicatorEntity.java
@@ -22,12 +22,51 @@ public class BdrHistoricalIndicatorEntity {
     @JoinColumn(name = "bdr_id")
     private BdrEntity bdr;
 
-    @Column(name = "nome_indicador")
-    private String nomeIndicador;
+    @Column(name = "year")
+    private Integer year;
 
-    @Column(name = "ano")
-    private Integer ano;
+    @Column(name = "pl", precision = 19, scale = 6)
+    private BigDecimal pl;
 
-    @Column(name = "valor", precision = 19, scale = 6)
-    private BigDecimal valor;
+    @Column(name = "pvp", precision = 19, scale = 6)
+    private BigDecimal pvp;
+
+    @Column(name = "psr", precision = 19, scale = 6)
+    private BigDecimal psr;
+
+    @Column(name = "p_ebit", precision = 19, scale = 6)
+    private BigDecimal pEbit;
+
+    @Column(name = "p_ebitda", precision = 19, scale = 6)
+    private BigDecimal pEbitda;
+
+    @Column(name = "p_ativo", precision = 19, scale = 6)
+    private BigDecimal pAtivo;
+
+    @Column(name = "roe", precision = 19, scale = 6)
+    private BigDecimal roe;
+
+    @Column(name = "roic", precision = 19, scale = 6)
+    private BigDecimal roic;
+
+    @Column(name = "roa", precision = 19, scale = 6)
+    private BigDecimal roa;
+
+    @Column(name = "margem_bruta", precision = 19, scale = 6)
+    private BigDecimal margemBruta;
+
+    @Column(name = "margem_operacional", precision = 19, scale = 6)
+    private BigDecimal margemOperacional;
+
+    @Column(name = "margem_liquida", precision = 19, scale = 6)
+    private BigDecimal margemLiquida;
+
+    @Column(name = "vpa", precision = 19, scale = 6)
+    private BigDecimal vpa;
+
+    @Column(name = "lpa", precision = 19, scale = 6)
+    private BigDecimal lpa;
+
+    @Column(name = "patrimonio_por_ativos", precision = 19, scale = 6)
+    private BigDecimal patrimonioPorAtivos;
 }
