cbbe65f201f2ced13c69e48077b11729b85cc1f2
rpsouza441 <rpsouza@hotmail.co.uk>
2025-09-24 12:29:30 -0300

    Refatora mapeamentos de BDR para evitar conflitos

--- tickerscraper/src/main/java/br/dev/rodrigopinheiro/tickerscraper/adapter/output/persistence/mapper/bdr/BdrFinancialStatementMapper.java
+++ tickerscraper/src/main/java/br/dev/rodrigopinheiro/tickerscraper/adapter/output/persistence/mapper/bdr/BdrFinancialStatementMapper.java
@@ -11,53 +11,64 @@ import br.dev.rodrigopinheiro.tickerscraper.domain.model.bdr.QualityValue;
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface BdrFinancialStatementMapper {
 
-    @Mappings({
-            @Mapping(target = "id", ignore = true),
-            @Mapping(target = "bdr", ignore = true)
-    })
-    BdrDreYearEntity toEntity(DreYear year);
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "bdr", ignore = true)
+    BdrDreYearEntity toDreEntity(DreYear year);
 
-    @InheritInverseConfiguration
-    DreYear toDomain(BdrDreYearEntity entity);
+    @InheritInverseConfiguration(name = "toDreEntity")
+    DreYear toDreDomain(BdrDreYearEntity entity);
 
     List<BdrDreYearEntity> toDreEntityList(List<DreYear> source);
 
     List<DreYear> toDreDomainList(List<BdrDreYearEntity> source);
 
-    @Mappings({
-            @Mapping(target = "id", ignore = true),
-            @Mapping(target = "bdr", ignore = true)
-    })
-    BdrBpYearEntity toEntity(BpYear year);
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "bdr", ignore = true)
+    BdrBpYearEntity toBpEntity(BpYear year);
 
-    @InheritInverseConfiguration
-    BpYear toDomain(BdrBpYearEntity entity);
+    @InheritInverseConfiguration(name = "toBpEntity")
+    BpYear toBpDomain(BdrBpYearEntity entity);
 
     List<BdrBpYearEntity> toBpEntityList(List<BpYear> source);
 
     List<BpYear> toBpDomainList(List<BdrBpYearEntity> source);
 
-    @Mappings({
-            @Mapping(target = "id", ignore = true),
-            @Mapping(target = "bdr", ignore = true)
-    })
-    BdrFcYearEntity toEntity(FcYear year);
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "bdr", ignore = true)
+    BdrFcYearEntity toFcEntity(FcYear year);
 
-    @InheritInverseConfiguration
-    FcYear toDomain(BdrFcYearEntity entity);
+    @InheritInverseConfiguration(name = "toFcEntity")
+    FcYear toFcDomain(BdrFcYearEntity entity);
 
     List<BdrFcYearEntity> toFcEntityList(List<FcYear> source);
 
     List<FcYear> toFcDomainList(List<BdrFcYearEntity> source);
 
-    QualityValueEmbeddable toEntity(QualityValue value);
+    default QualityValueEmbeddable toEmbeddable(QualityValue value) {
+        if (value == null) {
+            return null;
+        }
+        QualityValueEmbeddable embeddable = new QualityValueEmbeddable();
+        embeddable.setValor(value.getValor());
+        embeddable.setQuality(value.getQuality());
+        embeddable.setRaw(value.getRaw());
+        return embeddable;
+    }
 
-    QualityValue toDomain(QualityValueEmbeddable value);
+    default QualityValue toDomain(QualityValueEmbeddable value) {
+        if (value == null) {
+            return null;
+        }
+        QualityValue qualityValue = new QualityValue();
+        qualityValue.setValor(value.getValor());
+        qualityValue.setQuality(value.getQuality());
+        qualityValue.setRaw(value.getRaw());
+        return qualityValue;
+    }
 }
